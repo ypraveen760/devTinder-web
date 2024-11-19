a89cd676575d23bc98130688109ad553cec064cd
Praveen Yadav <ypraveen760@gmail.com>
2024-11-19 22:34:01 +0530

    added NavBar via Daisy UI

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    daisyui: {
+      themes: ["light", "dark", "cupcake"],
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
